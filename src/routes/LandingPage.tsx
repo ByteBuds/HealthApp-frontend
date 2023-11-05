ba29025812251d0cdd8b51af5843d5a8cf40194b
Naveen Joy <naveenjoy18@gmail.com>
2023-11-05 10:22:20 +0530

    ui final

--- src/routes/LandingPage.tsx
+++ src/routes/LandingPage.tsx
@@ -9,20 +9,20 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { styled } from '@mui/material/styles';
+import Link from '@mui/material/Link';
 import Avatar from '@mui/material/Avatar';
 import Badge from '@mui/material/Badge';
 import IconButton from '@mui/material/IconButton';
 import Stack from '@mui/material/Stack';
 
 import person1 from "../assets/person1.jpeg"
-import pill1 from "../assets/pill1.png"
-import pill2 from "../assets/pill2.png"
-
+import medicologo2 from "../assets/medicologo2.png"
 import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 import NextMedication from "../components/NextMedication";
 import Emergency from "../components/Emergency";
 import CallDoc from "../components/CallDoc";
 import NextVisit from "../components/NextVisit";
+
 
 const SmallAvatar = styled(Avatar)(({ theme }) => ({
     width: 22,
@@ -33,15 +33,31 @@ const SmallAvatar = styled(Avatar)(({ theme }) => ({
 const LandinPage = () => {
     return (
         <Grid sx={{ backgroundColor: "#d1e3ff", mb: "1rem", height: { xs: "fit-content", lg: "100vh" } }}>
-            <Grid container direction="column" sx={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", py: "1rem" }}>
+            <Grid container direction="row" sx={{ display: "flex", flexDirection: "row", justifyContent: "left", alignItems: "center", py: "1rem" }}>
+                <img src={medicologo2} width="100" height="100" />
+                <Typography sx={{ fontWeight: 800 }}>My Medico</Typography>
+                <Grid sx={{ display: "flex", flexDirection: "column", mx: "auto", justifyContent: "center", alignItems: "center" }}>
+                    <Typography sx={{ fontWeight: 500, mt: 2 }}>Hello, Allie Greer</Typography>
+                    <Typography sx={{ fontWeight: 600, fontSize: "1.5rem" }}>How is your health today?</Typography>
+                </Grid>
+
+                <Link href="/profile">
+                    <Avatar alt="img"
+                        src={person1}
+                        sx={{ width: 60, height: 60, ml: "auto" }} />
+                </Link>
+
+            </Grid>
+            {/* <Grid container direction="column" sx={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", py: "1rem" }}>
                 <Avatar alt="img"
                     src={person1}
                     sx={{ width: 120, height: 120 }} />
                 <Typography sx={{ fontWeight: 500, mt: 2 }}>Hello, Allie Greer</Typography>
                 <Typography sx={{ fontWeight: 600, fontSize: "1.5rem" }}>How is your health today?</Typography>
 
-            </Grid>
-            <Grid container sx={{ px: "1rem", mt: "3rem", display: "flex", flexDirection: { xs: "column", md: "row" }, justifyContent: { xs: "center", md: "space-between" }, }}>
+            </Grid> */}
+
+            <Grid container sx={{ px: "1rem", mt: "3rem", display: "flex", flexDirection: { xs: "column", md: "row" }, justifyContent: { xs: "center", md: "space-evenly" }, }}>
                 <Grid item xs={12} md={5}>
                     <NextMedication />
                 </Grid>
